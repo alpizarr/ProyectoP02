9b9f96dcba12cd9813b71539559359ecc1e1644e
alpizarr <158339231+alpizarr@users.noreply.github.com>
2024-03-20 14:56:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,35 @@ int interfaz()
 {
     while (true)
     {
-        switch (r) 
+        
         cout << "1.- " << " Ver lista de usuario " << endl;    
         cout << "2.- " << " Ver lista de publicaciones " << endl;    
         cout << "3.- " << " Entrar a perfil de un usuario " << endl;    
         cout << "4.- " << " Agregar un nuevo usuario " << endl;    
-        cout << "5.- " << " Salir "  << endl;
+        cout << "5.- " << " Salir "  << endl; 
         cin >> r;
-    } 
+        switch (r) 
+        {
+        case 1:
+        lu();
+        case 2:
+        Lp();
+        case 3:
+        EntrarPerfil();
+        case 4:
+        Nuevouser();
+        case 5:
+        break;
+        }
+    }
 }
+
+int EntrarPerfil()
+{
+    cout << " Ver lista de amigos " << endl;
+    cout << " Ver publicaciones " << endl;
+    cout << " Crear publicaciones " << endl;
+    cout << " Entrar a perfil de amigo " << endl;
+    cout << " Agrega a un amigo " << endl; 
+    cout << " Salir " << endl;
+} 
