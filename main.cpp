24ce942a32f710126bcd657aee4a57d11f1d4574
alpizarr <158339231+alpizarr@users.noreply.github.com>
2024-03-20 14:36:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,17 @@
 #include "usuario.h"
 #include "publicacion.h"
 using namespace std;
+int r;
+int interfaz()
+{
+    while (true)
+    {
+        switch (r) 
+        cout << "1.- " << " Ver lista de usuario " << endl;    
+        cout << "2.- " << " Ver lista de publicaciones " << endl;    
+        cout << "3.- " << " Entrar a perfil de un usuario " << endl;    
+        cout << "4.- " << " Agregar un nuevo usuario " << endl;    
+        cout << "5.- " << " Salir "  << endl;
+        cin >> r;
+    } 
+}
