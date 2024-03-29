8fca5b19344cdad6e4abd8d253086a454ded5615
alpizarr <158339231+alpizarr@users.noreply.github.com>
2024-03-29 10:18:14 -0600

    Update publicacion.h

--- publicacion.h
+++ publicacion.h
@@ -1,6 +1,24 @@
+
 #ifndef publicacion_h
 #define publicacion_h
 
 
+class Publicacion {
+private:
+  int id;
+  string contenido;
+  Usuario* autor;
+
+public:
+  Publicacion(int id, string contenido, Usuario* autor);
+
+  int getId() { return id; }
+
+  void mostrar();
+
+  // ... (otras funcionalidades)
+};
+
+#endif
 
 #endif
