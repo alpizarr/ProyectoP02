6e40ef0687ba12c66966af0d9f26040becd87237
alpizarr <158339231+alpizarr@users.noreply.github.com>
2024-03-29 10:18:30 -0600

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,6 +1,24 @@
 #ifndef usuario_h
 #define usuario_h
 
+class Usuario {
+private:
+  int id;
+  string nombre;
+  string biografia;
+  vector<Usuario*> amigos;
+  vector<Publicacion*> publicaciones;
+
+public:
+  Usuario(int id, string nombre, string biografia);
+
+  int getId() { return id; }
+  string getNombre() { return nombre; }
+
+  void mostrarPerfil();
+  void mostrarPublicaciones();
+
+};
 
 
 
