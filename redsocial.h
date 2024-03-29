e01c79342106fc03ee52e1886cb2cabc2a477adc
alpizarr <158339231+alpizarr@users.noreply.github.com>
2024-03-29 10:18:51 -0600

    Update redsocial.h

--- redsocial.h
+++ redsocial.h
@@ -1,5 +1,29 @@
 #ifndef redsocial_h
 #define redsocial_h
+#include <vector>
+#include "usuario.h"
+#include "publicacion.h"
+
+class RedSocial {
+private:
+  string nombre;
+  vector<Usuario*> usuarios;
+  vector<Publicacion*> publicaciones;
+  Usuario* usuarioActual;
+
+public:
+  RedSocial(string nombre);
+
+  void agregarUsuario(Usuario* usuario);
+  void mostrarUsuarios();
+  void mostrarPublicaciones();
+  Usuario* getUsuario(int id);
+
+  void mostrarPerfilUsuarioActual();
+  void mostrarAmigosUsuarioActual();
+  void crearPublicacion();
+
+};
 
 
 
